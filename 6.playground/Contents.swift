3b1b7c1be0b98e5c47fee4a65b911f6830cffc40
Peter Hajas <peterhajas@gmail.com>
2017-12-09 12:11:21 -0800

    Day 6 part 2

--- 6.playground/Contents.swift
+++ 6.playground/Contents.swift
@@ -34,44 +34,43 @@ extension Array where Element == [Int] {
     }
 }
 
+func redistributeConfiguration(_ configuration: [Int]) -> [Int] {
+    var mutableConfiguration = configuration
+    
+    // Find the highest value in the configuration
+    let highestValue = mutableConfiguration.highestValue
+    
+    // Remove the value at this index
+    mutableConfiguration[highestValue.index] = 0
+    
+    func configurationIndexAfterIndex(_ index: Int) -> Int {
+        var nextIndex: Int
+        if index == mutableConfiguration.indices.last {
+            nextIndex = 0
+        }
+        else {
+            nextIndex = mutableConfiguration.index(after: index)
+        }
+        return nextIndex
+    }
+    
+    // Redistribute evenly starting at the next index
+    
+    var indexToWriteAt = configurationIndexAfterIndex(highestValue.index)
+    for _ in 0..<highestValue.value {
+        mutableConfiguration[indexToWriteAt] += 1
+        indexToWriteAt = configurationIndexAfterIndex(indexToWriteAt)
+    }
+    
+    return mutableConfiguration
+}
+
 func numberOfRedistributionsBeforeDistributionOccursAgain(bank: [Int]) -> Int {
     var seenConfigurations = [[Int]]()
     var currentConfiguration = bank
     
-    func redistributeConfiguration(_ configuration: [Int]) -> [Int] {
-        var mutableConfiguration = configuration
-        
-        // Find the highest value in the configuration
-        let highestValue = mutableConfiguration.highestValue
-        
-        // Remove the value at this index
-        mutableConfiguration[highestValue.index] = 0
-        
-        func configurationIndexAfterIndex(_ index: Int) -> Int {
-            var nextIndex: Int
-            if index == mutableConfiguration.indices.last {
-                nextIndex = 0
-            }
-            else {
-                nextIndex = mutableConfiguration.index(after: index)
-            }
-            return nextIndex
-        }
-        
-        // Redistribute evenly starting at the next index
-        
-        var indexToWriteAt = configurationIndexAfterIndex(highestValue.index)
-        for _ in 0..<highestValue.value {
-            mutableConfiguration[indexToWriteAt] += 1
-            indexToWriteAt = configurationIndexAfterIndex(indexToWriteAt)
-        }
-        
-        return mutableConfiguration
-    }
-    
     while(true) {
         seenConfigurations.append(currentConfiguration)
-        print(seenConfigurations.count)
         
         // Redistribute the current configuration
         currentConfiguration = redistributeConfiguration(currentConfiguration)
@@ -91,4 +90,41 @@ print(numberOfRedistributionsBeforeDistributionOccursAgain(bank: sample)) // 5
 // Problem
 let problemInput = [5,1,10,0,1,7,13,14,3,12,8,10,7,12,0,6]
 print(numberOfRedistributionsBeforeDistributionOccursAgain(bank: problemInput)) // 5042
+
+// Part 2
+
+func sizeOfInfiniteLoop(_ bank: [Int]) -> Int {
+    var seenConfigurations = [[Int]]()
+    seenConfigurations.append(bank)
+    
+    // Go until we've seen a configuration before
+    while(true) {
+        var newConfiguration = redistributeConfiguration(seenConfigurations.last!)
+        if seenConfigurations.contains(newConfiguration) {
+            // We hit a loop! Break out
+            break
+        }
+        else {
+            seenConfigurations.append(newConfiguration)
+        }
+    }
+    
+    // Now that we've got our interesting configuration, keep redistributing
+    // until we see it again
+    var interestingConfiguration = seenConfigurations.last!
+    var sizeOfInfiniteLoop = 1
+    var runningConfiguration = redistributeConfiguration(interestingConfiguration)
+    while (true) {
+        if runningConfiguration == interestingConfiguration {
+            break
+        }
+        sizeOfInfiniteLoop += 1
+        runningConfiguration = redistributeConfiguration(runningConfiguration)
+    }
+    
+    return sizeOfInfiniteLoop
+}
+
+print(sizeOfInfiniteLoop(sample)) // 4
+print(sizeOfInfiniteLoop(problemInput)) // 1086
 
